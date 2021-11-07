7da685e9505ee5a5a66be37211c1215d80a6e194
Aditya5665 <89216177+Aditya5665@users.noreply.github.com>
2021-11-07 18:24:47 -0500

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,211 +1,100 @@
-var PLAY = 1;
-var END = 0;
-var gameState = PLAY;
-
-var trex, trex_running, trex_collided;
-var ground, invisibleGround, groundImage;
-
-var cloudsGroup, cloudImage;
-var obstaclesGroup, obstacle1, obstacle2, obstacle3, obstacle4;
-var backgroundImg
-var score=0;
-var jumpSound, collidedSound;
-
-var gameOver, restart;
-
+var background, ship;
+var planet, planet1, planet2, planet3, planet4, planet5;
+var alien, aliens, alien2, alien3;
+var lives = 3;
+var gameState = "PLAY";
 
 function preload(){
-  jumpSound = loadSound("jump.wav")
-  collidedSound = loadSound("collided.wav")
-  
-  backgroundImg = loadImage("backgroundImg.png")
-  sunAnimation = loadImage("sun.png");
-  
-  trex_running = loadAnimation("trex_2.png","trex_1.png","trex_3.png");
-  trex_collided = loadAnimation("trex_collided.png");
-  
-  groundImage = loadImage("ground.png");
-  
-  cloudImage = loadImage("cloud.png");
-  
-  obstacle1 = loadImage("obstacle1.png");
-  obstacle2 = loadImage("obstacle2.png");
-  obstacle3 = loadImage("obstacle3.png");
-  obstacle4 = loadImage("obstacle4.png");
-  
-  gameOverImg = loadImage("gameOver.png");
-  restartImg = loadImage("restart.png");
+    backgroundImg = loadImage("./assets/background.jpg");
+    shipImg = loadImage("./assets/ship.png");
+    planetImg1 = loadImage("./assets/planet.png");
+    planetImg2 = loadImage("./assets/planet1.png");
+    planetImg3 = loadImage("./assets/planet2.png");
+    planetImg4 = loadImage("./assets/planet3.png");
+    planetImg5 = loadImage("./assets/planet4.png");
+    planetImg6 = loadImage("./assets/planet5.png");
+    alienImg = loadImage("./assets/alien1.png");
+
+
 }
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  
-  sun = createSprite(width-50,100,10,10);
-  sun.addAnimation("sun", sunAnimation);
-  sun.scale = 0.6
-  
-  trex = createSprite(50,height-70,20,50);
-  
-  
-  trex.addAnimation("running", trex_running);
-  trex.addAnimation("collided", trex_collided);
-  trex.setCollider('circle',0,0,350)
-  trex.scale = 0.08
-  // trex.debug=true
-  
-  invisibleGround = createSprite(width/2,height-10,width,125);  
-  invisibleGround.shapeColor = "#f4cbaa";
-  
-  ground = createSprite(width/2,height,width,2);
-  ground.addImage("ground",groundImage);
-  ground.x = width/2
-  ground.velocityX = -(6 + 3*score/100);
-  
-  gameOver = createSprite(width/2,height/2- 50);
-  gameOver.addImage(gameOverImg);
-  
-  restart = createSprite(width/2,height/2);
-  restart.addImage(restartImg);
-  
-  gameOver.scale = 0.5;
-  restart.scale = 0.6;
+    createCanvas(1200,800);
+    background = createSprite(200,200,10,10);
+    background.addImage(backgroundImg);
+    //planets sprite start
+    planet1 = createSprite(700,50,10,10);
+    planet1.addImage(planetImg2);
+    planet1.scale = 0.2;
 
-  gameOver.visible = false;
-  restart.visible = false;
-  
- 
-  // invisibleGround.visible =false
+    planet3 = createSprite(600,500,10,10);
+    planet3.addImage(planetImg4);
+    planet3.scale = 0.2;
 
-  cloudsGroup = new Group();
-  obstaclesGroup = new Group();
-  
-  score = 0;
+    planet4 = createSprite(374,550,10,10);
+    planet4.addImage(planetImg5);
+    planet4.scale = 0.2;
+
+    planet5 = createSprite(100,100,10,10);
+    planet5.addImage(planetImg6);
+    planet5.scale = 0.2;
+
+    //planet sprite finish
+    ship = createSprite(30,320,50,50);
+    ship.addImage(shipImg);
+    ship.scale = 0.2;
+    aliens = createGroup();
+    createEdgeSprites();
 }
 
 function draw() {
-  //trex.debug = true;
-  background(backgroundImg);
-  textSize(20);
-  fill("black")
-  text("Score: "+ score,30,50);
-  
-  
-  if (gameState===PLAY){
-    score = score + Math.round(getFrameRate()/60);
-    ground.velocityX = -(6 + 3*score/100);
     
-    if((touches.length > 0 || keyDown("SPACE")) && trex.y  >= height-120) {
-      jumpSound.play( )
-      trex.velocityY = -10;
-       touches = [];
+    if(gameState === "PLAY"){
+        ship.y = World.mouseY;
+        //ship.bounceOff(edges);
+        spawnAliens();
+        
+        console.log(World.frameCount);
+        World.framerate = 60;
     }
     
-    trex.velocityY = trex.velocityY + 0.8
-  
-    if (ground.x < 0){
-      ground.x = ground.width/2;
+    if(ship.isTouching(aliens)){
+        aliens.destroyEach();
+        lives = lives - 1;
     }
-  
-    trex.collide(invisibleGround);
-    spawnClouds();
-    spawnObstacles();
-  
-    if(obstaclesGroup.isTouching(trex)){
-        collidedSound.play()
-        gameState = END;
-    }
-  }
-  else if (gameState === END) {
-    gameOver.visible = true;
-    restart.visible = true;
-    
-    //set velcity of each game object to 0
-    ground.velocityX = 0;
-    trex.velocityY = 0;
-    obstaclesGroup.setVelocityXEach(0);
-    cloudsGroup.setVelocityXEach(0);
-    
-    //change the trex animation
-    trex.changeAnimation("collided",trex_collided);
-    
-    //set lifetime of the game objects so that they are never destroyed
-    obstaclesGroup.setLifetimeEach(-1);
-    cloudsGroup.setLifetimeEach(-1);
-    
-    if(touches.length>0 || keyDown("SPACE")) {      
-      reset();
-      touches = []
-    }
-//  if(mousePressedOver(restart)){
-//    reset();
-//  } 
 
-  }
-  
-  
-  drawSprites();
+    if(lives === 0){
+        aliens.destroyEach();
+        gameState = "END";
+    }
+
+    drawSprites();
+    if(gameState === "END"){
+        ship.destroy();
+        fill("Red");
+        textSize(50);
+        text("Game Over! Try Again!", 150,300);
+    }
+    fill("white");
+    textSize(35);
+    text("Lives: "+ lives, 700,150);    
 }
 
-function spawnClouds() {
-  //write code here to spawn the clouds
-  if (frameCount % 60 === 0) {
-    var cloud = createSprite(width+20,height-300,40,10);
-    cloud.y = Math.round(random(100,220));
-    cloud.addImage(cloudImage);
-    cloud.scale = 0.5;
-    cloud.velocityX = -3;
-    
-     //assign lifetime to the variable
-    cloud.lifetime = 300;
-    
-    //adjust the depth
-    cloud.depth = trex.depth;
-    trex.depth = trex.depth+1;
-    
-    //add each cloud to the group
-    cloudsGroup.add(cloud);
+
+function spawnAliens() {
+    //write code here to spawn the aliens
+    if (frameCount % 100 === 0) {
+      alien = createSprite(800,100,40,10);
+      alien.addImage(alienImg)
+      alien.y = Math.round(random(10,500))
+      alien.scale = 0.2;
+      alien.velocityX = -10;
+      
+      
+      //assigning lifetime to the variable
+      alien.lifetime = 400;
+      
+        aliens.add(alien);
+      }
   }
   
-}
-
-function spawnObstacles() {
-  if(frameCount % 60 === 0) {
-    var obstacle = createSprite(600,height-95,20,30);
-    obstacle.setCollider('circle',0,0,45)
-    // obstacle.debug = true
-  
-    obstacle.velocityX = -(6 + 3*score/100);
-    
-    //generate random obstacles
-    var rand = Math.round(random(1,2));
-    switch(rand) {
-      case 1: obstacle.addImage(obstacle1);
-              break;
-      case 2: obstacle.addImage(obstacle2);
-              break;
-      default: break;
-    }
-    
-    //assign scale and lifetime to the obstacle           
-    obstacle.scale = 0.3;
-    obstacle.lifetime = 300;
-    obstacle.depth = trex.depth;
-    trex.depth +=1;
-    //add each obstacle to the group
-    obstaclesGroup.add(obstacle);
-  }
-}
-
-function reset(){
-  gameState = PLAY;
-  gameOver.visible = false;
-  restart.visible = false;
-  
-  obstaclesGroup.destroyEach();
-  cloudsGroup.destroyEach();
-  
-  trex.changeAnimation("running",trex_running);
-  
-  score = 0;
-  
-}
